ac851d593e29b59fc74318520e66ad5406a9674a
Justin Zamora <justinzamora02@gmail.com>
2022-04-04 11:36:17 +0800

    [iam] Add required parameter

--- iam/ci.tf
+++ iam/ci.tf
@@ -19,5 +19,6 @@ resource "aws_iam_access_key" "ci_runner" {
 
 resource "aws_iam_user_policy" "ci_test_essentials" {
   name   = "TestEssentials"
+  user   = aws_iam_user.ci_runner.name
   policy = data.aws_iam_policy_document.test_essentials.json
 }
