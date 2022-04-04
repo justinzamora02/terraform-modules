a8080968d1355b26af7549899b17fff3b159df34
Justin Zamora <justinzamora02@gmail.com>
2022-04-04 11:31:56 +0800

    [iam] Add CI test runner

---
+++ iam/ci.tf
@@ -0,0 +1,23 @@
+data "aws_iam_policy_document" "test_essentials" {
+  statement {
+    actions = [
+      "ec2:DescribeAccountAttributes",
+      "ec2:DescribeAvailabilityZones"
+    ]
+
+    resources = ["*"]
+  }
+}
+resource "aws_iam_user" "ci_runner" {
+  name = "CIRunner"
+  path = "/tools/"
+}
+
+resource "aws_iam_access_key" "ci_runner" {
+  user = aws_iam_user.ci_runner.name
+}
+
+resource "aws_iam_user_policy" "ci_test_essentials" {
+  name   = "TestEssentials"
+  policy = data.aws_iam_policy_document.test_essentials.json
+}
